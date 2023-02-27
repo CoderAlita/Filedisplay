fd6aa05429eb5502c8bb757f48bd12fae972fcaf
priyanka <priyankavisheshwar1018@gmail.com>
2023-02-27 19:18:01 +0530

    Final commit , markup added

--- server.py
+++ server.py
@@ -32,6 +32,13 @@ def getContent(file, start, end):
 
     return content[start:end],msg
 
+# Displaying Not Found page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
+
 @app.route("/display")
 def file_diaplay():
 
